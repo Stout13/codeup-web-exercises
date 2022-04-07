fab912900de734a3d86f00d4ddb405ee1ec3ffa7
Stout13 <stout.stephen13@gmail.com>
2022-04-07 10:43:20 -0500

    warmup string to freq map

---
+++ js/freq.js
@@ -0,0 +1,22 @@
+
+
+function frequency(input) {
+
+    var input = input.toLowerCase().split("");
+
+    dict = {};
+    var k = 0;
+    for (var i = 0; i < input.length; i++) {
+
+        // https://stackoverflow.com/questions/18690814/how-to-increment-an-object-property-value-if-it-exists-else-set-the-initial-val contributed by "snak" 09/09/2013
+        // dict input at i iterates through input and dict[input[i]] is true(exists) add 1, when dict[input[i]] is false or DNE dict[input[i]]
+        // which is the value of input array at i is created with a value 0
+
+            dict[input[i]] = (dict[input[i]] || 0) + 1;
+        }
+    return dict;
+}
+
+var things = "RaceCar";
+
+console.log(frequency(things));
