706c56d8569e6d142766a6d6c3937cb3fdd934eb
Stout13 <stout.stephen13@gmail.com>
2022-04-26 22:39:38 -0500

    jquery exercises1-2

--- JS/jquery-exercise.js
+++ JS/jquery-exercise.js
@@ -1,3 +1,18 @@
-$(document).ready(function() {
-    alert( 'The DOM has finished loading!' );
-});
+// $(document).ready(function() {
+//     alert( 'The DOM has finished loading!' );
+// });
+
+// var contents = $('#important').html();
+// alert(contents);
+
+// $('.codeup').css('border', '1px solid red');
+
+$('li').css('font-size', '20px');
+
+
+// COMBINE SELECTORS
+//VVVVVVVVVVVVVVV
+// $('h1', 'p', 'li');
+
+var contents = $('h1').html();
+alert(contents);
