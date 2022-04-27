8ebdce316147c39fb8483b4a7ffd758f1ae97c18
Stout13 <stout.stephen13@gmail.com>
2022-04-27 09:41:34 -0500

    wk8day3 exercise2

--- warmups-practice/wk8day3.js
+++ warmups-practice/wk8day3.js
@@ -10,3 +10,27 @@ function daysBetween(day1, day2) {
 }
 
 // It should return the number of days that lies between those dates
+
+// Write a function that takes an array (a) and a number (b) as arguments
+// Sum up all array elements with a value greater than b
+// Return the sum
+
+// example input: [1, 2, 3, 4, 5, 6, 7], 2
+// expected output: 25
+
+
+function arrBottomTrim(arr, num) {
+    var arrSum = 0;
+    for (var i = 0; i<arr.length; i++) {
+        if (arr[i] > num) {
+            arrSum = arrSum + arr[i];
+        }
+    }
+    return arrSum;
+}
+
+var arr= [1, 2, 3, 4, 5, 6, 7]
+var num= 2;
+
+arrBottomTrim(arr, num);
+
