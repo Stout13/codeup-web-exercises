4237cd863fddfd813749479dc14346f0bccbe7e3
Stout13 <stout.stephen13@gmail.com>
2022-05-02 16:35:09 -0500

    Treversing

--- JS/jquery_faq.js
+++ JS/jquery_faq.js
@@ -1,6 +1,12 @@
 
 
 $('dd').setAttribute('class', 'invisible');
+// $('li').setAttribute('class', 'highlighted');
+// $('h3').click(function() {
+//     // $('h3'.next()).children('li').css('background-color', 'red');
+//     console.log("I've been clicked");
+//     // $(this.children.each).css('font-weight', 'bold');
+// });
 // $('.invisible').css('visibility', 'hidden')
 
 //
